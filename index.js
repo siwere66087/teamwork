b45b360623586183377b8ab1b0a3b5eeaf39ca2d
ChrisanerM <chrisanermatthys04@gmail.com>
2022-11-28 15:04:13 +0200

    Chrisaner

---
+++ index.js
@@ -0,0 +1,35 @@
+
+let cars=
+    {
+    "name": "BMW",
+    "year": 2005,
+    "model": "convertible",
+    "transmission": "automatic"
+    ,
+    
+    "name": "Audi",
+    "year": 2010,
+    "model": "spider",
+    "transmission": "automatic",
+    
+
+    "name": "Kia",
+    "year": 2015,
+    "model": "convertible",
+    "transmission": "manuel",
+    }
+    
+    
+
+
+console.log(cars);
+
+
+// const car = JSON.parse(cars);
+
+
+// fetch('json/index.json').then((response)=>{
+//     console.log('resolved', response);
+//     }).catch((error)=>{
+//     console.log('rejected',error);
+//     });
