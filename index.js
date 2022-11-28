fb3ace2d3056a784f5ab65f19f0e93b8901ab736
siwere66087 <114493677+siwere66087@users.noreply.github.com>
2022-11-28 15:20:31 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,99 @@
+let cars = [
+    {
+ 
+   "name":"boozer cars",
+   "carName":"bmw",
+   "carModel":"coops",
+   "year":2011,
+   "transmission":"manual"
+ 
+ 
+ 
+    } ,
+ 
+ 
+    {
+ 
+     "name":"boozer cars",
+     "carName":"mazdar",
+     "carModel":"hatchbag",
+     "year":2005,
+     "transmission":"manual"
+   
+   
+   
+      } ,
+      {
+ 
+         "name":"boozer cars",
+         "carName":"audi",
+         "carModel":"spider",
+         "year":2022,
+         "transmission":"automatic"
+       
+       
+       
+          } 
+ 
+ ]
+
+
+console.log(cars);
+var parsed_cars = JSON.parse(cars);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
